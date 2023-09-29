d82066028caae262b2e943448ba8ca07cebe2242
BlankRiser <rschoudhary1999@gmail.com>
2023-09-29 21:55:26 +0530

    remove unimportant stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,66 +1,61 @@
+import './globals.css';
 
-import "./globals.css";
-
-import { Global, Navbar } from "~/components/layouts";
-import { Analytics } from "~/components/layouts/analytics";
-import { ScrollToTop } from "~/components/shared";
-import { generalSans, inter } from "~/utils/fonts";
+import { Global, Navbar } from '~/components/layouts';
+import { Analytics } from '~/components/layouts/analytics';
+import { ScrollToTop } from '~/components/shared';
+import { generalSans, inter } from '~/utils/fonts';
 
 export const metadata = {
-  title: "ram.codes",
-  description: "Portfolio of Ram Shankar",
-  openGraph: {
-    title: "ram.codes",
-    description: "Portfolio of Ram Shankar",
-    url: "https://ram.codes",
-    siteName: "ram.codes",
-    images: [
-      {
-        url: "https://ram.codes/og.png",
-        width: 1920,
-        height: 1080,
-      },
-    ],
-    locale: "en-US",
-    type: "website",
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
-  twitter: {
-    title: "ram.codes",
-    card: "summary_large_image",
-  },
-  icons: {
-    shortcut: "/favicon.png",
-  },
+	title: 'ram.codes',
+	description: 'Portfolio of Ram Shankar',
+	openGraph: {
+		title: 'ram.codes',
+		description: 'Portfolio of Ram Shankar',
+		url: 'https://ram.codes',
+		siteName: 'ram.codes',
+		images: [
+			{
+				url: 'https://ram.codes/og.png',
+				width: 1920,
+				height: 1080,
+			},
+		],
+		locale: 'en-US',
+		type: 'website',
+	},
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			'max-video-preview': -1,
+			'max-image-preview': 'large',
+			'max-snippet': -1,
+		},
+	},
+	twitter: {
+		title: 'ram.codes',
+		card: 'summary_large_image',
+	},
+	icons: {
+		shortcut: '/favicon.png',
+	},
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en" className={`${generalSans.variable} font-sans`}>
-      <head>
-        <Analytics />
-      </head>
-      <body className={`relative bg-stone-950 ${inter.className}`}>
-        <Navbar />
-        {/* mt-14 is used to offset the height of navbar */}
-        <Global>{children}</Global>
-        <ScrollToTop />
-        {/* <Footer /> */}
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang='en' className={`${generalSans.className} font-sans`}>
+			<head>
+				<Analytics />
+			</head>
+			<body className={`relative bg-stone-950 ${inter.className}`}>
+				<Navbar />
+				{/* mt-14 is used to offset the height of navbar */}
+				<Global>{children}</Global>
+				<ScrollToTop />
+				{/* <Footer /> */}
+			</body>
+		</html>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,20 @@
-"use client";
+'use client';
 
-
-import Hero from "~/components/pages/home/hero";
-import { Projects } from "~/components/pages/home/projects";
-import { RevealWords } from "~/components/shared/reveal-words";
-import { TechMarquee } from "~/components/shared/tech-marquee";
+import Hero from '~/components/pages/home/hero';
+import { Projects } from '~/components/pages/home/projects';
+import { RevealWords } from '~/components/shared/reveal-words';
+import { TechMarquee } from '~/components/shared/tech-marquee';
 
 export default function Home() {
-  return (
-    <div>
-      <Hero />
-      <div className="relative">
-        <TechMarquee />
-      </div>
-      <RevealWords sentence="now is a good time to show how powerful and continous the program is" />
-      <div className="px-2 md:px-8 mt-8">
+	return (
+		<div>
+			<Hero />
+			<div className='relative'>
+				<TechMarquee />
+			</div>
+			{/* <div className="px-2 md:px-8 mt-8">
         <Projects />
-      </div>
-    </div>
-  );
+      </div> */}
+		</div>
+	);
 }
-
-
