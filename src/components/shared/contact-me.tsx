f43bf91eb7ec16fb6478d5ee14455fe1d168d34a
BlankRiser <rschoudhary1999@gmail.com>
2023-11-12 15:23:49 +0530

    inital contact form

--- src/components/shared/contact-me.tsx
+++ src/components/shared/contact-me.tsx
@@ -1,3 +1,7 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
 import {
   Input,
   Slider,
@@ -10,6 +14,8 @@ import {
   SliderTrigger,
 } from "../ui";
 import { Button } from "../ui/button";
+import { cn } from "~/utils/text-transforms";
+import React from "react";
 
 export const ContactMe = () => {
   return (
@@ -27,20 +33,7 @@ export const ContactMe = () => {
               Please fill the form below to contact me.
             </SliderDescription>
           </SliderHeader>
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <label htmlFor="name" className="text-right">
-                Name
-              </label>
-              <Input id="name" value="Pedro Duarte" className="col-span-3" />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <label htmlFor="username" className="text-right">
-                Username
-              </label>
-              <Input id="username" value="@peduarte" className="col-span-3" />
-            </div>
-          </div>
+          <ContactForm />
           <SliderFooter>
             <SliderClose asChild>
               <Button type="submit">Contact Me</Button>
@@ -51,3 +44,91 @@ export const ContactMe = () => {
     </div>
   );
 };
+
+const contactFormFieldSchema = z.object({
+  name: z.string().min(1, { message: "Name is required" }),
+  email: z.string().min(1, { message: "Email is required" }).email({
+    message: "Must be a valid email",
+  }),
+  company: z
+    .string()
+    .min(1, { message: "Please enter the name of your company" }),
+  // budget: z.number().int().min(0).max(1000000),
+  // service: z
+  //   .enum(["design", "development", "consulting"])
+  //   .default("development"),
+  // projectDetails: z.string(),
+});
+
+type ContactFormFields = z.infer<typeof contactFormFieldSchema>;
+
+const ContactForm = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<ContactFormFields>({
+    resolver: zodResolver(contactFormFieldSchema),
+  });
+
+  return (
+    <form onSubmit={handleSubmit((d) => console.log(d))}>
+      <div className="grid gap-4 py-4">
+        <ContactInput
+          label={"Name"}
+          type="text"
+          {...register("name", { required: true })}
+          error={errors.name?.message}
+        />
+        <ContactInput
+          label={"Email"}
+          placeholder="e.g. example.mail@example.com"
+          {...register("email", { required: true })}
+          error={errors.email?.message}
+        />
+        <ContactInput
+          label={"Company"}
+          placeholder="e.g. acme.inc"
+          {...register("company", { required: true })}
+          error={errors.company?.message}
+        />
+      </div>
+      <button type="submit">save it</button>
+    </form>
+  );
+};
+
+type ContactInputProps = {
+  label: string;
+  error?: string;
+} & React.InputHTMLAttributes<HTMLInputElement>;
+
+const ContactInput = React.forwardRef<HTMLInputElement, ContactInputProps>(
+  ({ label, error, ...rest }, ref) => {
+    return (
+      <div
+        className={cn([
+          " flex flex-col rounded-md border border-neutral-600 bg-neutral-800 px-2 py-1 focus-within:border ",
+          error
+            ? "focus-within:border-red-400 border-red-400"
+            : "focus-within:border-devhaven-500",
+        ])}
+      >
+        <label htmlFor="name" className="p-1 text-left">
+          {label}
+        </label>
+        <Input
+          border={false}
+          focus={false}
+          id="name"
+          placeholder="e.g. Van Dyke"
+          className="outline-none"
+          {...rest}
+        />
+        {error && <span className="text-sm">{error}</span>}
+      </div>
+    );
+  }
+);
+ContactInput.displayName = "ContactInput";
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,23 +1,58 @@
-import React from 'react';
-import { cn } from '~/utils/text-transforms';
+import { VariantProps, cva } from "class-variance-authority";
+import React from "react";
+import { cn } from "~/utils/text-transforms";
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+const inputVariants = cva(
+  "flex rounded-md bg-transparent ring-offset-transparent file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-600  disabled:cursor-not-allowed disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default: "bg-transparent",
+      },
+      inputSize: {
+        default: "h-10 w-full px-3 py-2 text-sm",
+        sm: "h-8 w-full px-3 py-2 text-sm",
+        lg: "w-full px-2 py-1 text-sm",
+      },
+      border: {
+        true: "border border-neutral-800",
+      },
+      focus: {
+        true: "focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-primary focus-visible:ring-offset-1",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      inputSize: "default",
+      border: true,
+      focus: true,
+    },
+  }
+);
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement>,
+    VariantProps<typeof inputVariants> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-	({ className, type, ...props }, ref) => {
-		return (
-			<input
-				type={type}
-				className={cn(
-					'flex h-10 w-full rounded-md border border-neutral-800 bg-transparent px-3 py-2 text-sm ring-offset-transparent file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-600 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-primary focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50',
-					className,
-				)}
-				ref={ref}
-				{...props}
-			/>
-		);
-	},
+  ({ className, border, focus, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          inputVariants({
+            className,
+            border: border,
+            focus: focus,
+          }),
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
 );
-Input.displayName = 'Input';
+Input.displayName = "Input";
 
 export { Input };
