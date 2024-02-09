f640f53dbf0b4dfa950bbbec37f1dc2d6a25e6dd
BlankRiser <rschoudhary1999@gmail.com>
2024-02-09 18:27:09 +0530

    fix: wrap articles in suspense boundary

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,15 +1,16 @@
-import { allBlogs } from "contentlayer/generated";
-import { Footer, NavbarOffset } from "~/components/layouts";
-import ArticleList from "~/components/pages/blog/article-list";
-import { ContentHeader } from "~/components/shared/header";
+import { allBlogs } from 'contentlayer/generated';
+import { Suspense } from 'react';
+import { Footer, NavbarOffset } from '~/components/layouts';
+import ArticleList from '~/components/pages/blog/article-list';
+import { ContentHeader } from '~/components/shared/header';
 
 export default async function BlogPage() {
   return (
-    <div className="mx-auto w-full max-w-7xl px-2 py-8">
-      <NavbarOffset className="h-6 md:h-28" />
+    <div className='mx-auto w-full max-w-7xl px-2 py-8'>
+      <NavbarOffset className='h-6 md:h-28' />
       <section>
         <ContentHeader
-          title={"Articles"}
+          title={'Articles'}
           description={`Explore a variety of articles where I dive into topics like code
             refactoring, software development, CI/CD pipelines, version control,
             web-dev practices, scalability, and secure coding. Whether
@@ -19,9 +20,11 @@ export default async function BlogPage() {
         />
       </section>
 
-      <section className="mx-auto grid w-full max-w-4xl place-items-center">
-        <ArticleList allArticles={allBlogs} />
-      </section>
+      <Suspense fallback={<>loading</>}>
+        <section className='mx-auto grid w-full max-w-4xl place-items-center'>
+          <ArticleList allArticles={allBlogs} />
+        </section>
+      </Suspense>
     </div>
   );
 }
