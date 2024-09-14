d10999e120a5226c8d1382557da551c44160cdc9
BlankRiser <rschoudhary1999@gmail.com>
2024-09-14 11:44:41 +0530

    chore: custom 404 page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,38 @@
-'use client';
+"use client"
 
-import Cal from '@calcom/embed-react';
-import { MotionValue, useScroll } from 'framer-motion';
-import { useRef } from 'react';
-import { Hero } from '~/components/pages/home/hero';
-import { TechMarquee } from '~/components/shared/tech-marquee';
+import Cal from "@calcom/embed-react"
+import { MotionValue, useScroll } from "framer-motion"
+import { useRef } from "react"
+import { Hero } from "~/components/pages/home/hero"
+import { TechMarquee } from "~/components/shared/tech-marquee"
 
 export default function Home() {
-  const container = useRef<HTMLDivElement | null>( null );
+	const container = useRef<HTMLDivElement | null>(null)
 
-  const { scrollYProgress } = useScroll( {
-    target: container,
-    offset: [ 'start start', 'end end' ],
-  } );
+	const { scrollYProgress } = useScroll({
+		target: container,
+		offset: ["start start", "end end"],
+	})
 
-  return (
-    <div ref={container}>
-      <div className='sticky top-0'>
-        <Hero scrollYProgress={scrollYProgress} />
-      </div>
-      <div className='relative z-[999]'>
-        <Projects scrollYProgress={scrollYProgress} />
-      </div>
-
-      <TechMarquee />
-    </div>
-  );
+	return (
+		<div ref={container}>
+			<div className="sticky top-0">
+				<Hero scrollYProgress={scrollYProgress} />
+			</div>
+			<div className="relative z-[999]">
+				<Projects scrollYProgress={scrollYProgress} />
+			</div>
+			<TechMarquee />
+		</div>
+	)
 }
 
 const Projects: React.FC<{
-  scrollYProgress: MotionValue<number>;
-}> = ( { scrollYProgress } ) => {
-  return (
-    <div className='grid min-h-[100vh] w-full place-items-center overflow-visible bg-neutral-900'>
-      <Cal calLink='ram-codes' className='w-full'></Cal>
-    </div>
-  );
-};
+	scrollYProgress: MotionValue<number>
+}> = ({ scrollYProgress }) => {
+	return (
+		<div className="grid min-h-[100vh] w-full place-items-center overflow-visible bg-neutral-900">
+			<Cal calLink="ram-codes" className="w-full"></Cal>
+		</div>
+	)
+}
--- src/components/layouts/global.tsx
+++ src/components/layouts/global.tsx
@@ -1,34 +1,34 @@
-'use client';
+"use client"
 
-import { motion } from 'framer-motion';
-import React, { useEffect } from 'react';
-import Lenis from 'lenis';
+import { motion } from "framer-motion"
+import React, { useEffect } from "react"
+import Lenis from "lenis"
 
 export function Global({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    const lenis = new Lenis();
+	useEffect(() => {
+		const lenis = new Lenis()
 
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
+		function raf(time: number) {
+			lenis.raf(time)
+			requestAnimationFrame(raf)
+		}
 
-    requestAnimationFrame(raf);
-  }, []);
+		requestAnimationFrame(raf)
+	}, [])
 
-  return (
-    <motion.main
-      initial={{ y: 10 }}
-      animate={{ y: 0 }}
-      transition={{
-        duration: 0.25,
-        type: 'spring',
-        stiffness: 100,
-        damping: 20,
-      }}
-      className='z-10'
-    >
-      {children}
-    </motion.main>
-  );
+	return (
+		<motion.main
+			initial={{ y: 10 }}
+			animate={{ y: 0 }}
+			transition={{
+				duration: 0.25,
+				type: "spring",
+				stiffness: 100,
+				damping: 20,
+			}}
+			className="z-10"
+		>
+			{children}
+		</motion.main>
+	)
 }
